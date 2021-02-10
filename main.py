8c862e546613754a24c54296685593eb5979b509
Mugesh Kannan <mugeshk1171@gmail.com>
2021-02-10 17:33:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import torch
 import librosa
 
 tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
-
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
 speech, sr =librosa.load('test.wav')
@@ -11,5 +10,6 @@ speech = librosa.resample(speech, sr, 16000)
 input_values = tokenizer(speech, return_tensors='pt').input_values
 logits = model(input_values).logits
 predict_id = torch.argmax(logits, dim = -1)
+
 transcription = tokenizer.decode(predict_id[0])
 print(transcription)
